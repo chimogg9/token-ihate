f74d132aee5c6c2321ef88557a3607ceeff365b2
chimogg9 <sergiovelalf@gmail.com>
2025-03-13 02:47:44 +0100

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
     "address": "J9fsqHzQAwiPAFsoBb9ePy7dJbH2Ho1ds2NSw9W45FGx",
     "symbol": "IHATE",
     "name": "I Hate Rug Pull... :(",
-    "uri": "https://scarlet-fantastic-gerbil-794.mypinata.cloud/ipfs/bafkreidqcdnhtqwdc55dytpdt6wmunlflnkyoaiqcaumlnvaebr7nf4ftm",
+    "uri": "https://raw.githubusercontent.com/chimogg9/token-ihate/refs/heads/main/js.js",
     "decimals": 9,
     "image": "https://imgur.com/ZBnOlM1",  // Asegúrate de subir la imagen y obtener la URL
     "description": "I hate Rug Pulls... :/ #RugPullNoMore",
