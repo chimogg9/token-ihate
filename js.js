9d00887c7367751a7ac3c7ca75748117602443a2
chimogg9 <sergiovelalf@gmail.com>
2025-03-13 02:46:24 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+{
+    "address": "J9fsqHzQAwiPAFsoBb9ePy7dJbH2Ho1ds2NSw9W45FGx",
+    "symbol": "IHATE",
+    "name": "I Hate Rug Pull... :(",
+    "uri": "https://scarlet-fantastic-gerbil-794.mypinata.cloud/ipfs/bafkreidqcdnhtqwdc55dytpdt6wmunlflnkyoaiqcaumlnvaebr7nf4ftm",
+    "decimals": 9,
+    "image": "https://imgur.com/ZBnOlM1",  // Asegúrate de subir la imagen y obtener la URL
+    "description": "I hate Rug Pulls... :/ #RugPullNoMore",
+    "attributes": [
+        {
+            "trait_type": "Color",
+            "value": "Rojo"
+        },
+        {
+            "trait_type": "Rareza",
+            "value": "Común"
+        }
+    ]
+}
