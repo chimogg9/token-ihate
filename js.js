b8be44ff70b52d69c32ad24efa5eb9366f34b7eb
chimogg9 <sergiovelalf@gmail.com>
2025-03-13 03:32:27 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,19 +1,10 @@
 {
-    "address": "HA1xNvKT5ZX6vgYYkkDoQf1bJ4PTkagJYT31yKnFtRoZ",
-    "symbol": "IHATE",
-    "name": "I Hate Rug Pull... :(",
-    "uri": "https://raw.githubusercontent.com/chimogg9/token-ihate/main/js.js",
-    "decimals": 9,
-    "image": "https://imgur.com/ZBnOlM1",  // Asegúrate de subir la imagen y obtener la URL
-    "description": "I hate Rug Pulls... :/ #RugPullNoMore",
-    "attributes": [
-        {
-            "trait_type": "Color",
-            "value": "Rojo"
-        },
-        {
-            "trait_type": "Rareza",
-            "value": "Común"
-        }
-    ]
+  "name": "I Hate Rug Pull... :(",
+  "symbol": "IHATE",
+  "description": "I Hate Rug Pull... :(",
+  "image": "https://imgur.com/ZBnOlM1",
+  "uri": "https://raw.githubusercontent.com/chimogg9/token-ihate/main/js.js",
+  "extensions": {
+    "telegram": "t.me/ihaterugpull"
+  }
 }
